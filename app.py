b6feed624a3fdd759713cceb45ca8c6607ffeb36
Prabhanshu <rajputprabhanshuchauhan@gmail.com>
2020-03-27 03:26:04 +0530

    Saved Model Deployed

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-import digit_recognition
+from keras.models import model_from_json
 from flask import Flask, render_template, redirect, request
-
+import numpy as np
 app = Flask(__name__)
+
+json_file = open('model.json', 'r')
+model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(model_json)
+loaded_model.load_weights('model.h5')
 
 @app.route('/')
 def home():
@@ -19,10 +25,10 @@ def pred():
         li_X.append(0)
         arr_X = np.array(li_X)
         arr_X = np.reshape(arr_X, (1,28,28,1))
-        ans = classifier.predict(arr_X)
+        ans = loaded_model.predict(arr_X)
         ans = np.argmax(ans, axis = 1)
         return str(ans)
-    return "ok"
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=False)
