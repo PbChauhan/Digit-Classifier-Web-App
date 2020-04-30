6cb668b4d8907be19afef17a356291bc8c246d6a
Prabhanshu <rajputprabhanshuchauhan@gmail.com>
2020-04-30 23:03:27 +0530

    Changes to image size added and new user interface added

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from keras.models import model_from_json
 from flask import Flask, render_template, redirect, request
 import matplotlib.pyplot as plt
 import numpy as np
+import cv2
 app = Flask(__name__)
+
 
 json_file = open('model.json', 'r')
 model_json = json_file.read()
@@ -10,8 +12,11 @@ json_file.close()
 loaded_model = model_from_json(model_json)
 loaded_model.load_weights('model.h5')
 
-@app.route('/')
+@app.route('/', methods = ['GET','POST'])
 def home():
+    if request.method == "POST":
+        y = request.form.get('val')
+        print(y)
     return render_template('index.html')
 
 @app.route('/predict', methods = ['GET', 'POST'])
@@ -24,14 +29,15 @@ def pred():
             if i.isnumeric():
                li_X.append(i) 
         li_X.append(0)
-        arr_X = np.array(li_X, dtype = 'float')
-        plt.imsave("predict.png", np.reshape(arr_X, (1,28*4,28*4))[0,:,:])
-        arr_X = np.reshape(arr_X, (1,28*4,28*4,1))
+        arr_X = np.reshape(np.array(li_X, dtype = 'float'), (1,28*4,28*4))[0,:,:]
+        arr_X = cv2.resize(arr_X, (28*1,28*1))
+        plt.imsave("./static/image/predict.png", arr_X)
+        arr_X = np.reshape(arr_X, (1,28*1,28*1,1))
         ans = loaded_model.predict(arr_X)
         ans = np.argmax(ans, axis = 1)
         return str(ans)
     return redirect('/')
 
-app.run(debug=True, use_reloader=False)
+app.run(debug=True, use_reloader=False, threaded = False)
     
 
